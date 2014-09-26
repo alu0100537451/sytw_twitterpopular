175e9ed72b50242ca9a99f8522369b19eb027200
alu0100537451 <alu0100537451@ull.edu.es>
2014-09-26 09:12:40 +0100

    Update twitter.rb

--- twitter.rb
+++ twitter.rb
@@ -3,39 +3,39 @@ require 'sinatra'
 require './configure'
 
 get '/' do  
-  @fc= 0 # número de amigos
+  @n_amigos = 0 # número de amigos
   @name = ''   # nombre de usuario twitter
   @usuarios = Hash.new	#almacenara los amigos y el numero de seguidores
   erb :twitter
 end
 
 post '/' do  
-  @fc = 0   
+  @n_amigos = 0   
   @usuarios = Hash.new
-  @name = params[:firstname] || ''  #recoge del parametro firstname el nombre de usuario
-  client = my_twitter_client()  #establece conexion twitter
+  @name = params[:firstname] || ''  
+  client = my_twitter_client()  
   
-  if client.user? @name #si el usuario introducido es de twitter 
+  if client.user? @name 
     
-    a_user = client.user(@name) 
-    @fc = a_user.friends_count #recoge el numero de amigos del usuario
+    a_user = client.user(@name)  
+    @n_amigos = a_user.friends_count #recoge el numero de amigos del usuario
     amigos = client.friend_ids(@name).attrs[:ids].take(10) #muestra 10 amigos del usuario por el campo id
     
-    if (@fc < 10)        #si tiene menos de diez amigos
-        @fc.times do |i| #muestra 1 a 1
+    if (@n_amigos < 10) 
+        @n_amigos.times do |i| 
             user_n = client.user(amigos[i])
-            @usuarios[user_n.screen_name.to_s] = user_n.followers_count.to_i 
+            @usuarios[user_n.screen_name.to_s] = user_n.followers_count.to_i
         end 
         
     end
-    if (@fc >= 10) #si tiene mas de diez amigos  
+    if (@n_amigos >= 10)   
         10.times do |i|
             user_n = client.user(amigos[i])
             @usuarios[user_n.screen_name.to_s] = user_n.followers_count.to_i
         end         
     end 
     
-    @usuarios = @usuarios.sort_by {|k,v| -v} #ordena los amigos de mayor a menor segun sus seguidores              
+    @usuarios = @usuarios.sort_by {|k,v| -v} #te muestras los usuarios ordenados             
   end
   erb :twitter
 end
